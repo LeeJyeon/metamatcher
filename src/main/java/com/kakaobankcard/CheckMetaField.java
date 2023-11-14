a01b6a8194d4f4c235c945994e17c66c942f36bd
LeeJyeon <jihyun6969@nate.com>
2023-11-14 22:20:28 +0900

    commit

---
+++ src/main/java/com/kakaobankcard/CheckMetaField.java
@@ -0,0 +1,12 @@
+package com.kakaobankcard;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+import org.jetbrains.annotations.NotNull;
+
+public class CheckMetaField extends AnAction {
+    @Override
+    public void actionPerformed(@NotNull AnActionEvent e) {
+
+    }
+}
